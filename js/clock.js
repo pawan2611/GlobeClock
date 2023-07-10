4dac0078c662c11b85a38c3893987c6c4977598e
Pawan Kumar <112843736+pawan2611@users.noreply.github.com>
2023-07-10 21:28:25 +0530

    initial commit
    uploading all the files

---
+++ js/clock.js
@@ -0,0 +1,334 @@
+function valToName(value) {
+  switch (value) {
+    case "Etc/GMT+12":
+      selectedName = "International Date Line West";
+      break;
+    case "Pacific/Midway":
+      selectedName = "Midway Island, Samoa";
+      break;
+    case "Pacific/Honolulu":
+      selectedName = "Hawaii";
+      break;
+    case "US/Alaska":
+      selectedName = "Alaska";
+      break;
+    case "America/Los_Angeles":
+      selectedName = "Pacific Time (US & Canada)";
+      break;
+    case "America/Tijuana":
+      selectedName = "Tijuana, Baja California";
+      break;
+    case "US/Arizona":
+      selectedName = "Arizona";
+      break;
+    case "America/Chihuahua":
+      selectedName = "Chihuahua, La Paz, Mazatlan";
+      break;
+    case "US/Mountain":
+      selectedName = "Mountain Time (US & Canada)";
+      break;
+    case "America/Managua":
+      selectedName = "Central America";
+      break;
+    case "US/Central":
+      selectedName = "Central Time (US & Canada)";
+      break;
+    case "America/Mexico_City":
+      selectedName = "Guadalajara, Mexico City, Monterrey";
+      break;
+    case "Canada/Saskatchewan":
+      selectedName = "Saskatchewan";
+      break;
+    case "America/Bogota":
+      selectedName = "Bogota, Lima, Quito, Rio Branco";
+      break;
+    case "US/Eastern":
+      selectedName = "Eastern Time (US & Canada)";
+      break;
+    case "US/East-Indiana":
+      selectedName = "Indiana (East)";
+      break;
+    case "Canada/Atlantic":
+      selectedName = "Atlantic Time (Canada)";
+      break;
+    case "America/Caracas":
+      selectedName = "Caracas, La Paz";
+      break;
+    case "America/Manaus":
+      selectedName = "Manaus";
+      break;
+    case "America/Santiago":
+      selectedName = "Santiago";
+      break;
+    case "Canada/Newfoundland":
+      selectedName = "Newfoundland";
+      break;
+    case "America/Sao_Paulo":
+      selectedName = "Brasilia";
+      break;
+    case "America/Argentina/Buenos_Aires":
+      selectedName = "Buenos Aires, Georgetown";
+      break;
+    case "America/Godthab":
+      selectedName = "Greenland";
+      break;
+    case "America/Montevideo":
+      selectedName = "Montevideo";
+      break;
+    case "America/Noronha":
+      selectedName = "Mid-Atlantic";
+      break;
+    case "Atlantic/Cape_Verde":
+      selectedName = "Cape Verde Is";
+      break;
+    case "Atlantic/Azores":
+      selectedName = "Azores";
+      break;
+    case "Africa/Casablanca":
+      selectedName = "Casablanca, Monrovia, Reykjavik";
+      break;
+    case "Etc/Greenwich":
+      selectedName = "Greenwich Mean Time : Dublin, Edinburgh, Lisbon, London";
+      break;
+    case "Europe/Amsterdam":
+      selectedName = "Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna";
+      break;
+    case "Europe/Belgrade":
+      selectedName = "Belgrade, Bratislava, Budapest, Ljubljana, Prague";
+      break;
+    case "Europe/Brussels":
+      selectedName = "Brussels, Copenhagen, Madrid, Paris";
+      break;
+    case "Europe/Sarajevo":
+      selectedName = "Sarajevo, Skopje, Warsaw, Zagreb";
+      break;
+    case "Africa/Lagos":
+      selectedName = "West Central Africa";
+      break;
+    case "Asia/Amman":
+      selectedName = "Amman";
+      break;
+    case "Europe/Athens":
+      selectedName = "Athens, Bucharest, Istanbul";
+      break;
+    case "Asia/Beirut":
+      selectedName = "Beirut";
+      break;
+    case "Africa/Cairo":
+      selectedName = "Cairo";
+      break;
+    case "Africa/Harare":
+      selectedName = "Harare, Pretoria";
+      break;
+    case "Europe/Helsinki":
+      selectedName = "Helsinki, Kyiv, Riga, Sofia, Tallinn, Vilnius";
+      break;
+    case "Asia/Jerusalem":
+      selectedName = "Jerusalem";
+      break;
+    case "Europe/Minsk":
+      selectedName = "Minsk";
+      break;
+    case "Africa/Windhoek":
+      selectedName = "Windhoek";
+      break;
+    case "Asia/Kuwait":
+      selectedName = "Kuwait, Riyadh, Baghdad";
+      break;
+    case "Europe/Moscow":
+      selectedName = "Moscow, St. Petersburg, Volgograd";
+      break;
+    case "Africa/Nairobi":
+      selectedName = "Nairobi";
+      break;
+    case "Asia/Tbilisi":
+      selectedName = "Tbilisi";
+      break;
+    case "Asia/Tehran":
+      selectedName = "Tehran";
+      break;
+    case "Asia/Muscat":
+      selectedName = "Abu Dhabi, Muscat";
+      break;
+    case "Asia/Baku":
+      selectedName = "Baku";
+      break;
+    case "Asia/Yerevan":
+      selectedName = "Yerevan";
+      break;
+    case "Asia/Kabul":
+      selectedName = "Kabul";
+      break;
+    case "Asia/Yekaterinburg":
+      selectedName = "Yekaterinburg";
+      break;
+    case "Asia/Karachi":
+      selectedName = "Islamabad, Karachi, Tashkent";
+      break;
+    case "Asia/Calcutta":
+      selectedName = "Chennai, Kolkata, Mumbai, New Delhi";
+      break;
+    case "Asia/Calcutta":
+      selectedName = "Sri Jayawardenapura";
+      break;
+    case "Asia/Katmandu":
+      selectedName = "Kathmand";
+      break;
+    case "Asia/Almaty":
+      selectedName = "Almaty, Novosibirsk";
+      break;
+    case "Asia/Dhaka":
+      selectedName = "Astana, Dhaka";
+      break;
+    case "Asia/Rangoon":
+      selectedName = "Yangon (Rangoon)";
+      break;
+    case "Asia/Bangkok":
+      selectedName = "Bangkok, Hanoi, Jakarta";
+      break;
+    case "Asia/Krasnoyarsk":
+      selectedName = "Krasnoyarsk";
+      break;
+    case "Asia/Hong_Kong":
+      selectedName = "Beijing, Chongqing, Hong Kong, Urumqi";
+      break;
+    case "Asia/Kuala_Lumpur":
+      selectedName = "Kuala Lumpur, Singapore";
+      break;
+    case "Asia/Irkutsk":
+      selectedName = "Irkutsk, Ulaan Bataar";
+      break;
+    case "Australia/Perth":
+      selectedName = "Perth";
+      break;
+    case "Asia/Taipei":
+      selectedName = "Taipei";
+      break;
+    case "Asia/Tokyo":
+      selectedName = "Osaka, Sapporo, Tokyo";
+      break;
+    case "Asia/Seoul":
+      selectedName = "Seoul";
+      break;
+    case "Asia/Yakutsk":
+      selectedName = "Yakutsk";
+      break;
+    case "Australia/Adelaide":
+      selectedName = "Adelaide";
+      break;
+    case "Australia/Darwin":
+      selectedName = "Darwin";
+      break;
+    case "Australia/Brisbane":
+      selectedName = "Brisbane";
+      break;
+    case "Australia/Canberra":
+      selectedName = "Canberra, Melbourne, Sydney";
+      break;
+    case "Australia/Hobart":
+      selectedName = "Hobart";
+      break;
+    case "Pacific/Guam":
+      selectedName = "Guam, Port Moresby";
+      break;
+    case "Asia/Vladivostok":
+      selectedName = "Vladivostok";
+      break;
+    case "Asia/Magadan":
+      selectedName = "Magadan, Solomon Is., New Caledonia";
+      break;
+    case "Pacific/Auckland":
+      selectedName = "Auckland, Wellington";
+      break;
+    case "Pacific/Fiji":
+      selectedName = "Fiji, Kamchatka, Marshall Is.";
+      break;
+    case "Pacific/Tongatapu":
+      selectedName = "Nuku'alofa";
+      break;
+    default:
+      break;
+  }
+  return selectedName;
+}
+
+function alarm(currentTime) {
+  alarmDateTime = document.getElementById("alarmform").value;
+  const date = new Date(alarmDateTime);
+
+  const options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true
+  };
+
+  formattedDatetime = date.toLocaleDateString('en-US', options);
+
+  if (currentTime == formattedDatetime) {
+    document.getElementById('alarmfinal').innerHTML = '';
+    alarmSound = new Audio('alarm.mp3');
+    alarmSound.play();
+  }
+}
+
+document.getElementById('stopalarm').addEventListener('click', () => {
+  alarmSound.pause();
+})
+
+document.getElementById('alarmbtn').addEventListener('click', () => {
+  document.getElementById('alarmfinal').innerHTML = 'Alarm Set For ' + formattedDatetime + '.';
+})
+
+document.querySelector('#changeTimeZone').addEventListener('change', detectTimeZone)
+tzName = document.querySelector('#tzname').textContent
+
+let changedTimeZone;
+
+function detectTimeZone(event) {
+  clearInterval(initalSetTime);
+  changedTimeZone = document.querySelector('#changeTimeZone').value;
+  changedTimeZoneName = valToName(changedTimeZone);
+  document.querySelector('#tzname').textContent = changedTimeZoneName;
+  console.log(event);
+
+  changedSetTime = setInterval(() => {
+    cdt = new Date();
+    newDateTime = cdt.toLocaleString('en-US', {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      timeZone: changedTimeZone
+    })
+    alarm(newDateTime);
+    document.getElementById("time").innerHTML = newDateTime;
+  }, 1000)
+}
+
+
+
+initalSetTime = setInterval(() => {
+  idt = new Date();
+  document.querySelector('#tzname').textContent = 'Chennai, Kolkata, Mumbai, New Delhi'
+  timeZone = "Asia/Calcutta";
+  newDateTime = idt.toLocaleString('en-US', {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    timeZone: timeZone
+  })
+  alarm(newDateTime);
+  document.getElementById("time").innerHTML = newDateTime;
+}, 1000)
